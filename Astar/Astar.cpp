84d6c2bbf5d8cd0189ed6354a6a320e8860873ce
MrTroble <nifroh56@gmail.com>
2020-11-24 20:12:30 +0100

    Added heuristic

--- Astar/Astar.cpp
+++ Astar/Astar.cpp
@@ -6,6 +6,7 @@
 #include "tgengine/TGEngine/public/pipeline/CommandBuffer.hpp"
 #include <array>
 #include <thread>
+#include <vector>
 
 using namespace std;
 using namespace tge::tex;
@@ -18,39 +19,162 @@ bool textureChanged = false;
 bool wait = true;
 bool lock = false;
 
-#define WAIT() wait = false; while(!wait) continue
+#define RUN
+#ifdef RUN
+#define WAIT() textureChanged = true; while(textureChanged) continue
+#else
+#define WAIT() textureChanged = true; wait = false; while(!wait) continue
+#endif
+
+inline uint8_t* getEntry(const size_t x, const size_t y) {
+	return texture.data + ((y * texture.x + x) * 4);
+}
+
+inline uint8_t* getEntry(const glm::vec2 pos) {
+	return getEntry(pos.x, pos.y);
+}
+
+constexpr uint8_t white[] = { 0xFF, 0xFF, 0xFF, 0xFF };
+constexpr uint8_t red[] = { 0xFF, 0, 0, 0xFF };
+constexpr uint8_t green[] = { 0, 0xFF, 0, 0xFF };
+constexpr uint8_t blue[] = { 0, 0, 0xFF, 0xFF };
+constexpr uint8_t black[] = { 0, 0, 0, 0xFF };
+
+inline void setColor(uint8_t* ptr, const uint8_t* color) {
+	memcpy(ptr, color, 4);
+}
+
+inline bool isColor(const uint8_t* ptr, const uint8_t* color) {
+	return memcmp(ptr, color, 4) == 0;
+}
+
+inline bool isWhite(const uint8_t* ptr) {
+	return isColor(ptr, white);
+}
+
+inline bool isBlack(const uint8_t* ptr) {
+	return isColor(ptr, black);
+}
+
+struct NavData;
+
+struct NavPath {
+	uint32_t next = -1;
+	uint32_t heuristic = -1;
+};
+
+struct NavData {
+	NavPath possiblePaths[4];
+};
+
+inline void getColorFromHeuristics(const NavData& data, uint8_t* colorout) {
+	uint32_t color = 0;
+	for (uint32_t i = 0; i < sizeof(data.possiblePaths) / sizeof(NavPath); i++)
+		color += data.possiblePaths[i].heuristic;
+	color = std::pow(color, 7);
+	memcpy(colorout, &color, 3);
+	colorout[3] = 0xFF;
+}
 
 static void runAstar() {
 	WAIT();
 
-    constexpr uint8_t white[] = { 0xFF, 0xFF, 0xFF, 0xFF };
-	constexpr uint8_t green[] = { 0, 0xFF, 0, 0xFF };
-	constexpr uint8_t hg[] = { 0, 0xF0, 0, 0xFF };
 
+	glm::vec2 startpoint = { 0, 0 };
 	for (size_t i = 0; i < texture.x; i++) {
-		if (memcmp(texture.data + i * 4, white, 4) == 0) {
-			memcpy(texture.data + i * 4, green, 4);
+		uint8_t* ptr = getEntry(i, 0);
+		if (isWhite(ptr)) {
+			memcpy(ptr, green, 4);
+			startpoint.x = i;
 			break;
 		}
 	}
 
-	textureChanged = true;
 	WAIT();
 
+	glm::vec2 endpoint = { 0, texture.y };
 	for (size_t i = 0; i < texture.x; i++) {
-		uint8_t* ptr = texture.data + i * 4 + (texture.x * (texture.y - 1) * 4);
-		if (memcmp(ptr, white, 4) == 0) {
-			memcpy(ptr, hg, 4);
+		uint8_t* ptr = getEntry(i, texture.y - 1);
+		if (isWhite(ptr)) {
+			memcpy(ptr, blue, 4);
+			endpoint.x = i;
 			break;
 		}
 	}
 
-	textureChanged = true;
+	WAIT();
+
+	std::vector<glm::vec2> candidatePositions;
+	const size_t predictionSize = texture.y * texture.x / 2;
+	candidatePositions.reserve(predictionSize);
+
+	struct Directions {
+		bool right;
+		bool left;
+		bool up;
+		bool down;
+	};
+	std::vector<Directions> candidateDirections;
+	candidateDirections.reserve(predictionSize);
+
+	for (size_t y = 1; y < texture.y - 1; y++) {
+		for (size_t x = 1; x < texture.x - 1; x++) {
+			uint8_t* ptr = getEntry(x, y);
+			if (!isWhite(ptr)) continue;
+			const bool right = !isBlack(ptr + 4);
+			const bool left = !isBlack(ptr - 4);
+			const bool up = !isBlack(getEntry(x, y + 1));
+			const bool down = !isBlack(getEntry(x, y - 1));
+			if (((right || left) && (up || down) ||
+				(((int)right + (int)left + (int)up + (int)down) == 1))) {
+				memcpy(ptr, red, 4);
+				candidatePositions.push_back({ x, y });
+				candidateDirections.push_back({ right, left, up, down});
+			}
+		}
+		WAIT();
+	}
+
+	WAIT();
+
+	std::vector<NavData> navpath;
+	navpath.reserve(predictionSize);
+
+	for (size_t i = 0; i < candidatePositions.size(); i++) {
+		NavData navdata;
+		const glm::vec2 pos = candidatePositions[i];
+		const Directions dir = candidateDirections[i];
+
+		const auto begin = candidatePositions.begin();
+		const auto end = candidatePositions.end();
+
+		for (uint32_t j = 0; j < candidatePositions.size(); j++) {
+			const glm::vec2 p = candidatePositions[j];
+			const bool onX = p.x == pos.x;
+			const bool onY = p.y == pos.y;
+			if (!onX && !onY) continue;
+			int diffx = p.x - pos.x;
+			int diffy = p.y - pos.y;
+			if (dir.right && diffx > 0 && navdata.possiblePaths[0].heuristic > diffx)
+				navdata.possiblePaths[0] = { j, (uint32_t)diffx };
+			if (dir.left && diffx < 0 && navdata.possiblePaths[1].heuristic >(diffx *= -1))
+				navdata.possiblePaths[1] = { j, (uint32_t)diffx };
+			if (dir.up && diffy > 0 && navdata.possiblePaths[2].heuristic > diffy)
+				navdata.possiblePaths[2] = { j, (uint32_t)diffy };
+			if (dir.down && diffy < 0 && navdata.possiblePaths[3].heuristic >(diffy *= -1))
+				navdata.possiblePaths[3] = { j, (uint32_t)diffy };
+		}
+		uint8_t colorHeur[4];
+		getColorFromHeuristics(navdata, colorHeur);
+		setColor(getEntry(pos), colorHeur);
+		navpath.push_back(navdata);
+		WAIT();
+	}
+
 	WAIT();
 }
 
-int main()
-{
+int main() {
 	initEngine();
 
 	SamplerInputInfo inputInfo;
@@ -62,7 +186,7 @@ int main()
 	createSampler(inputInfo, &currentMap.sampler);
 
 	createdMaterials = new Material[255];
-	createdMaterials[0] = { { 1, 1, 1, 1}, 0};
+	createdMaterials[0] = { { 1, 1, 1, 1}, 0 };
 
 	texture.data = stbi_load("canvas.png", &texture.x, &texture.y, &texture.comp, 0);
 
@@ -73,7 +197,7 @@ int main()
 	thread.detach();
 
 	playercontroller = [](Input input) {
-		if(input.inputX == 0) lock = false;
+		if (input.inputX == 0) lock = false;
 		if (lock == true)
 			return;
 		if (input.inputX > 0 && wait != true) {
@@ -93,7 +217,7 @@ int main()
 	};
 
 	actorDescriptor.push_back({ 6, 0, 0, UINT32_MAX });
-	actorProperties.push_back({ { glm::fmat4(1), 0, 0 }, 0, 0});
+	actorProperties.push_back({ { glm::fmat4(1), 0, 0 }, 0, 0 });
 
 	BufferInputInfo bufferInputInfos[4];
 	bufferInputInfos[0].flags = VK_SHADER_STAGE_ALL_GRAPHICS;
